49af57b9aedda1004029162eb152f0fbc9361a7f
Rosendo Garcia <109121364+Roosendo@users.noreply.github.com>
2023-06-19 16:56:36 -0600

    create and add everything for the logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import freeCodeCampLogo from './imgs/freeCodeCamp-logo.png';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          src={freeCodeCampLogo}
+          className='freecodecamp-logo'
+          alt='Logo de freeCodeCamp' />
+      </div>
     </div>
   );
 }
